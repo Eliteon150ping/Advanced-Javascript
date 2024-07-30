9610da170a0e2739a7093c1838507544fe582f48
SussyBakaHere <rishensurjoobhalee@gmail.com>
2024-07-30 18:49:51 +0200

    Chapter 12 reuploaded

---
+++ Chapter 12/regex.js
@@ -0,0 +1,86 @@
+// let text = "I love JavaScript!";
+//  console.log(text.match(/javascript/i));
+
+//  let text = "I love React and JavaScript!";
+//  console.log(text.match(/javascript|nodejs|react/gi));
+
+//  let text = "Just some text.";
+//  console.log(text.match(/\./g));
+
+//  let text = "I'm 29 years old.";
+//  console.log(text.match(/\d/));
+
+//  let text = "Coding is a lot of fun!";
+//  console.log(text.match(/\s/g));
+
+// let text = "In the end or at the beginning?";
+//  console.log(text.match(/\bin/gi));
+
+//  let text = "I love JavaScript!";
+//  console.log(text.match(/(love|dislike)\s(javascript|spiders)/gi));
+
+
+//  let text = "I love JavaScript!";
+//  console.log(text.match(/[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z09]/g));
+
+//  let text = "You are doing great!";
+//  console.log(text.match(/n?g/gi));
+
+// let text = "123123123";
+//  console.log(text.match(/(123)+/));
+
+// let text = "123123123a";
+// console.log(text.match(/(123)*a+/));
+
+// let text = "abcabcabc";
+// console.log(text.match(/(abc){1,2}/));
+
+// let text = "I love JavaScript!";
+// console.log(text.match(/(?<language>javascript)/i));
+
+// let text = "That's not the case.";
+// console.log(text.search(/Case/i));
+
+// let text = "Coding is fun. Coding opens up a lot of opportunities.";
+//  console.log(text.replace("Coding", "JavaScript"))
+
+// let text = "Coding is fun. Coding opens up a lot of opportunities.";
+//  console.log(text.replace(/Coding/g, "JavaScript"));
+
+
+// let emailPattern = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._]+)/g;
+// let validEmail = "maaike_1234@email.com";
+// let invalidEmail = "maaike@mail@.com";
+// console.log(validEmail.match(emailPattern));
+// console.log(invalidEmail.match(emailPattern));
+
+// function test(a, b, c) {
+//     console.log("first:", a, arguments[0]);
+//     console.log("second:", b, arguments[1]);
+//     console.log("third:", c, arguments[2]);
+// }
+// test("fun", "js", "secrets");
+
+
+
+// function test(a, b, c) {
+//     a = "nice";
+//     arguments[1] = "JavaScript";
+//     console.log("first:", a, arguments[0]);
+//     console.log("second:", b, arguments[1]);
+//     console.log("third:", c, arguments[2]);
+// }
+// test("fun", "js", "secrets");
+
+
+// var x;
+//  x = 5;
+//  console.log(x);
+
+//  x = 5;
+//  console.log(x);
+//  var x;                  // Using 'let' will not work 
+
+
+
+    
