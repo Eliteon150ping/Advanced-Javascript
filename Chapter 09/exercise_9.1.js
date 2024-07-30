456cdb3c3bd98067081beabfa8ceabc6c428d366
SussyBakaHere <rishensurjoobhalee@gmail.com>
2024-07-30 18:48:42 +0200

    Chapter 9 reuploaded

---
+++ Chapter 09/exercise_9.1.js
@@ -0,0 +1,7 @@
+console.dir(window);              // Opens up a window displaying info about a website
+console.dir(window.document);     // Brings the info about the document
+console.dir(window.innerHeight);  // Finds inner height of window in pixels
+console.dir(window.innerWidth);   // Finds inner width of window in pixels
+console.dir(window.outerHeight);  // Finds outer height of window in pixels
+console.dir(window.outerWidth);   // Finds outer width of window in pixels
+
---
+++ Chapter 09/exercise_9.2.js
@@ -0,0 +1,4 @@
+console.dir(window.location);             // Finds the location of the current file
+console.dir(window.location.protocol);    // Finds the protocol in the location of the current file
+console.dir(window.location.href);        // Finds the href in the location of the current file
+
