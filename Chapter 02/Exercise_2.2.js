b107aeed24cfe24c6ee7363a5ec75a8bf5ccbfd2
SussyBakaHere <rishensurjoobhalee@gmail.com>
2024-07-30 18:43:58 +0200

    Chapter 2 reuploaded

---
+++ Chapter 02/Exercise_2.2.js
@@ -0,0 +1,12 @@
+let myName = "Rishen";
+let myAge = 18;
+let coder = true;
+let message = "Hello, my name is " + myName + ", I am " + myAge+" years old and I can code JavaScript: " + coder + "."; 
+console.log(message);
+
+myName = "Rishen";
+myAge = 18;
+coder = false;
+message = "Hello, my name is " + myName + ", I am " + myAge+" years old and I can code JavaScript: " + coder + "."; 
+console.log(message);
+
---
+++ Chapter 02/Exercise_2.3.js
@@ -0,0 +1,6 @@
+let a = prompt("Value 1?");
+let b = prompt("Value 2?");
+a = Number(a);
+b = Number(b);
+let hypotenuseVal = ((a * a) + (b * b))**0.5;
+console.log(hypotenuseVal);
